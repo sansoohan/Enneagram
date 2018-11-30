6224b37787a1381ac3089ce1604112ac05b78fae
Han SanSoo <sansoohan>
2018-11-30 11:28:39 +0900

    20181130

--- RecommendAndAdvertising/AdExample/src/environments/environment.prod.ts
+++ RecommendAndAdvertising/AdExample/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAu9EISZI3ZI3FXqc3CW0gmoHou19rd8FQ',
+    authDomain: 'chat-demo-5d3a7-5fded.firebaseapp.com',
+    databaseURL: 'https://chat-demo-5d3a7-5fded.firebaseio.com',
+    projectId: 'chat-demo-5d3a7-5fded',
+    storageBucket: 'chat-demo-5d3a7.appspot.com',
+    messagingSenderId: '47826805548'
+  }
 };
