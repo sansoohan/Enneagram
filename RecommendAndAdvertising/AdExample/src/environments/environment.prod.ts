8697f4c28afd1a29b28de85359d36ce6f2519d84
Han SanSoo <sansoohan>
2018-12-01 15:25:00 +0900

    20181201

--- RecommendAndAdvertising/AdExample/src/environments/environment.prod.ts
+++ RecommendAndAdvertising/AdExample/src/environments/environment.prod.ts
@@ -1,11 +1,5 @@
+import * as FirebaseKey from './server_key';
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: 'AIzaSyAu9EISZI3ZI3FXqc3CW0gmoHou19rd8FQ',
-    authDomain: 'chat-demo-5d3a7-5fded.firebaseapp.com',
-    databaseURL: 'https://chat-demo-5d3a7-5fded.firebaseio.com',
-    projectId: 'chat-demo-5d3a7-5fded',
-    storageBucket: 'chat-demo-5d3a7.appspot.com',
-    messagingSenderId: '47826805548'
-  }
+  firebase: FirebaseKey
 };
---
+++ RecommendAndAdvertising/AdExample/src/environments/server_key.ts
@@ -0,0 +1,8 @@
+export const FirebaseKey = {
+    apiKey: 'AIzaSyAu9EISZI3ZI3FXqc3CW0gmoHou19rd8FQ',
+    authDomain: 'chat-demo-5d3a7-5fded.firebaseapp.com',
+    databaseURL: 'https://chat-demo-5d3a7-5fded.firebaseio.com',
+    projectId: 'chat-demo-5d3a7-5fded',
+    storageBucket: 'chat-demo-5d3a7.appspot.com',
+    messagingSenderId: '47826805548'
+};
